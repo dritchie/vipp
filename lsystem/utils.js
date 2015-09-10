13511b8a25f87eb45bb6eaa5a27f4b6d0e805385
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-09-10 14:14:33 -0700

    WebPPL reference: using MCMC to generate approximately bilaterally-symmetric tree shapes.

--- lsystem/utils.js
+++ lsystem/utils.js
@@ -23,7 +23,8 @@ function render(canvas, viewport, branches) {
 	// Draw
 	ctx.strokeStyle = 'black';
 	ctx.lineCap = 'round';
-	branches.forEach(function(branch) {
+	for (var i = 0; i < branches.length; i++) {
+		var branch = branches[i];
 		var istart = world2img(branch.start);
 		var iend = world2img(branch.end);
 		var iwidth = branch.width / (viewport.xmax - viewport.xmin) * canvas.width;
@@ -32,7 +33,7 @@ function render(canvas, viewport, branches) {
 		ctx.moveTo(istart.x, istart.y);
 		ctx.lineTo(iend.x, iend.y);
 		ctx.stroke();
-	});
+	}
 }
 
 function renderOut(filename, res, viewport, branches) {
@@ -42,8 +43,75 @@ function renderOut(filename, res, viewport, branches) {
 }
 
 
+function ImageData2D(canvas) {
+	this.data = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height).data;
+	this.width = canvas.width;
+	this.height = canvas.height;
+}
+ImageData2D.prototype = {
+	constructor: ImageData2D,
+	getPixel: function(x, y) {
+		var i = y*this.width + x;
+		return [this.data[4*i], this.data[4*i+1], this.data[4*i+2], this.data[4*i+3]];
+	},
+	getLuminance: function(x, y) {
+		var i = y*this.width + x;
+		return 0.2126*this.data[4*i] + 0.7152*this.data[4*i+1] + 0.0722*this.data[4*i+2];
+	},
+	isFilled: function(x, y) {
+		var i = y*this.width + x;
+		return this.data[4*i] !== 255 || this.data[4*i+1] !== 255 || this.data[4*i+2] !== 255; 
+	},
+	percentFilled: function() {
+		var filled = 0;
+		for (var y = 0; y < this.height; y++) {
+			for (var x = 0; x < this.width; x++) {
+				filled += this.isFilled(x, y);
+			}
+		}
+		return filled / (this.width * this.height);
+	},
+	bilateralSymmetryHoriz: function() {
+		// Horizonal bilateral symmetry
+		var sim = 0;
+		for (var y = 0; y < this.height; y++) {
+			for (var x = 0; x < this.width / 2; x++) {
+				var f1 = this.isFilled(x, y);
+				var f2 = this.isFilled(this.width - x - 1, y);
+				sim += (f1 === f2);
+			}
+		}
+		return sim / (0.5 * this.width * this.height);
+	},
+	filledBilateralSymmetryHoriz: function() {
+		// Horizonal bilateral symmetry
+		var sim = 0;
+		var n = 0;
+		for (var y = 0; y < this.height; y++) {
+			for (var x = 0; x < this.width / 2; x++) {
+				var f1 = this.isFilled(x, y);
+				var f2 = this.isFilled(this.width - x - 1, y);
+				if (f1 || f2) {
+					sim += (f1 === f2);
+					n++;
+				}
+			}
+		}
+		return sim / n;
+	}
+};
+
 
 module.exports = {
 	render: render,
-	renderOut: renderOut
+	renderOut: renderOut,
+	newImageData2D: function(canvas) { return new ImageData2D(canvas); },
+	newCanvas: function(w, h) { return new Canvas(w, h); },
+	require: require	// so that webppl code can just require whatever code it wants
 };
+
+
+
+
+
+
