be5f3f03abc348b5678a693242a0b47a32268af3
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-09-15 11:35:58 -0700

    Switched nn input normalization to centering + standardization.

--- src/neuralnet/utils.js
+++ src/neuralnet/utils.js
@@ -25,15 +25,26 @@ function collectSample(callsite, inputs, inputCache) {
 	if (cacheEntry === undefined) {
 		cacheEntry = {
 			n: 0,
-			mins: numeric.rep([inputs.length], Number.MAX_VALUE),
-			maxs: numeric.rep([inputs.length], -Number.MAX_VALUE)
+			// mins: numeric.rep([inputs.length], Number.MAX_VALUE),
+			// maxs: numeric.rep([inputs.length], -Number.MAX_VALUE)
+			means: numeric.rep([inputs.length], 0),
+			variances: numeric.rep([inputs.length], 0)
 		};
 		inputCache.cache[callsite] = cacheEntry;
 	}
 	if (cacheEntry.n < inputCache.nToCollect) {
 		cacheEntry.n++;
-		numeric.mineq(cacheEntry.mins, inputs);
-		numeric.maxeq(cacheEntry.maxs, inputs);
+		// numeric.mineq(cacheEntry.mins, inputs);
+		// numeric.maxeq(cacheEntry.maxs, inputs);
+		var prevmeans = cacheEntry.means;
+		cacheEntry.means = numeric.add(prevmeans, numeric.div( numeric.sub(inputs, prevmeans), cacheEntry.n ));
+		var prevvar = cacheEntry.variances;
+		cacheEntry.variances = numeric.add(prevvar, numeric.mul( numeric.sub(inputs, prevmeans), numeric.sub(inputs, cacheEntry.means) ));
+		if (cacheEntry.n === inputCache.nToCollect) {
+			// cacheEntry.ranges = numeric.sub(cacheEntry.maxs, cacheEntry.mins);
+			numeric.diveq(cacheEntry.variances, cacheEntry.n - 1);
+			cacheEntry.stddevs = numeric.sqrt(cacheEntry.variances);
+		}
 	}
 }
 
@@ -41,10 +52,8 @@ function collectSample(callsite, inputs, inputCache) {
 function normalizeInputs(callsite, inputs, inputCache) {
 	var cacheEntry = inputCache.cache[callsite];
 	assert(cacheEntry !== undefined, 'Attempting to normalize NN inputs for which no sample stats exist!');
-	var newinputs = inputs.slice();
-	for (var i = 0; i < inputs.length; i++)
-		newinputs[i] = 2 * (inputs[i] - cacheEntry.mins[i]) / (cacheEntry.maxs[i] - cacheEntry.mins[i]) - 1;
-	return newinputs;
+	// return numeric.diveq(numeric.sub(inputs, cacheEntry.mins), cacheEntry.ranges);
+	return numeric.diveq(numeric.sub(inputs, cacheEntry.means), cacheEntry.stddevs);
 }
 
 
